4e69fd0e29d4f66f149f59fe710887e87ff7bcf2
Arunav Gupta <arunav1101@gmail.com>
2019-03-28 03:09:42 -0700

    update file

--- liri.js
+++ liri.js
@@ -1,6 +1,11 @@
 var axios = require("axios");
 var omdbApi = require('omdb-client');
 var Spotify = require('node-spotify-api');
+var moment = require('moment');
+var fs = require('fs');
+var path = require('path')
+var os = require('os');
+var _ = require('lodash');
 
 //***************API's */
 
@@ -15,79 +20,111 @@ var Spotify = require('node-spotify-api');
 //  [DotEnv](https://www.npmjs.com/pacnpm i dotenvkage/dotenv)
 //******************** */
 
-function grab(data) {
-    let value = process.argv.indexOf(data);
-    return (value === -1) ? null : process.argv[value + 1];
+//********logger************ */
+function appendFile(command, value) {
+    fs.appendFile(path.join(__dirname, 'log.txt'), `${command} : ${value} ` + os.EOL, (err) => {
+        if (err) throw err;
+    });
+}
+//********logger************ */
+
+function readFile() {
+    let fileData = fs.readFileSync(path.join(__dirname, 'random.txt'), 'utf8').split(':');
+    return fileData;
 }
 
-
-let liriRequest = grab(process.argv[2]);
-// let concertRequest = grab('concert');
-// let movieRequest = grab('movie');
-console.log(liriRequest);
-
 // ********************* OMDB*********************
-function searchMovies(movies) {
+function searchMovies(movies = 'alien') {
+    appendFile(process.argv[2], movies);
     var params = {
         apiKey: 'trilogy',
-        title: movies,
+        title: movies.trim(),
         year: 2012
     }
     omdbApi.get(params, function (err, data) {
-        console.log(data);
+        console.log(`* Title of the movie is ${data.Title}`);
+        console.log(`* IMDB Rating of the movie is ${data.imdbRating}`);
+        console.log(`* Rotten Tomatoes Rating of the movie ${data.Ratings[0].value}`);
+        console.log(`* Country where the movie was produced is ${data.Country}`);
+        console.log(`* Language of the movie is ${data.Language}`);
+        console.log(`* Plot of the movie is ${data.Plot}`);
+        console.log(`* Actors in the movie are ${data.Actors}`);
     });
-
 }
+
+/// ************format Address from lat long******
+function formatAddress(latlong) {
+    var queryURL = `https://api.opencagedata.com/geocode/v1/json?q=${latlong}&key=2bb14724541d469fbc31188506fb3e6f`;
+    const formattedAdress = axios({
+        url: queryURL,
+        method: "GET"
+    }).then(address => address.data.results[0].formatted);
+    return formattedAdress;
+}
+
 // ********************OMDB*********************
 
 // ********************Bands in Town for concerts***************
 
-function searchConcerts(concerts) {
-    let artist = concerts;
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+function searchConcerts(concerts = 'pink') {
+    let artist = concerts.trim();
+    appendFile(process.argv[2], artist);
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp&limit=1";
     axios({
         url: queryURL,
         method: "GET"
-    }).then(function (response) {
-
-        // Printing the entire object to console
-        console.log(response.data);
+    }).then(concerts => {
+        //   console.log(latLongValue)
+        //    concerts.data.map(  concert => {
+        //   console.log(`Latlong====>${concert.venue.latitude},${concert.venue.longitude}`)
+        // const promise = 
+        formatAddress(`${concerts.data[0].venue.latitude},${concerts.data[0].venue.longitude}`).then(address => {
+            console.log(`\n
+    ********************************************************\n
+ *Venue Name: ${concerts.data[0].venue.name}\n\n*Venue:${address}\n
+ *Date of the Event: ${moment(concerts.data[0].datetime).format('LLLL')}\n
+    ********************************************************`)
+            // return promise;
+            // })
+            // });
+        });
+    }).catch(err => {
+        console.log('Search for another concert!!');
     });
 }
 // ********************Bands in Town for concerts***************
 
 
 // ********************Spotify for songs*************** 
-function songSearch(songs) {
+function songSearch(songs = 'The Sign') {
+    appendFile(process.argv[2], songs);
     var spotify = new Spotify({
         id: '42c2e2c6fb9c438ab666209481661822',
         secret: '6080687f959f4a86ac8db3b0728b3fab'
     });
     spotify.search({
         type: 'track',
-        query: songs
+        query: songs.trim()
     }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log(data);
+        console.log(`\n\n\n * The Name of song is : ${(data.tracks.items[0].name).toUpperCase()}`);
+        console.log(`* The Artist of song is : ${(data.tracks.items[0].artists[0].name).toUpperCase()}`);
+        console.log(`* The Album of song is : ${(data.tracks.items[0].album.name).toUpperCase()}`);
+        console.log(`* The Preview Url of song is : ${data.tracks.items[0].preview_url}`);
     });
 }
 // ********************Spotify for songs**************
 
-
-switch (process.argv[2]) {
-    case ('spotify-this-song'):
-        searchMovies(process.argv[3].trim());
-        break;
-    case ('concert-this'):
-        searchConcerts(process.argv[3].trim());
-        break;
-    case ('movie-this'):
-        songSearch(process.argv[3].trim());
-        break;
-
-    case ('do-what-it-says'):
-        songSearch(process.argv[3].trim());
-        break;
+let demand = {
+    'movie-this': (movie = 'alien') => searchMovies(movie),
+    'concert-this': (concert = 'pink') => searchConcerts(concert),
+    'spotify-this-song': (song = 'honest') => songSearch(song),
+    'do-what-it-says': () => {
+        const newData = readFile();
+        demand[newData[0].trim()](newData[1].trim())
+    }
 }
+
+demand[process.argv[2]]((process.argv[3]) || (''))
